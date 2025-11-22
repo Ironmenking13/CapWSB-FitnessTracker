ff776466fe98e051e817de4ebf1f3f1554996657
Krzysztof <kkubas20@wp.pl>
2025-11-22 09:31:32 +0100

    After Moving

--- src/main/java/pl/wsb/fitnesstracker/healthmetrics/HealthMetrics.java
+++ src/main/java/pl/wsb/fitnesstracker/healthmetrics/HealthMetrics.java
@@ -1,11 +1,16 @@
 package pl.wsb.fitnesstracker.healthmetrics;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
+import jakarta.annotation.Nullable;
+import jakarta.persistence.*;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.ToString;
+import pl.wsb.fitnesstracker.user.api.User;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.List;
 
 @Entity
 @Table(name = "HealthMetrics")
@@ -13,6 +18,28 @@ import lombok.ToString;
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @ToString
 public class HealthMetrics {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Nullable
+    private Long id;
+
+    @Column(name = "user_id", nullable = true, unique = true)
+    @OneToMany
+    @JoinColumn(name = "user_id")
+    private List<User> user_id;
+
+    @Column(name = "date", nullable = false)
+    private LocalDateTime date;
+
+    @Column(name = "weight", nullable = true)
+    private LocalDate weight;
+
+    @Column(name = "height", nullable = true)
+    private LocalDate height;
+
+    @Column(name = "hearthRate", nullable = true)
+    private LocalDate hearthRate;
 
 
 }
